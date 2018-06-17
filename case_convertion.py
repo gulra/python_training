8417ff6faf10daca537f9f7ef630fe936917cb48
gulra <ragulraja0404@gmail.com>
2018-06-17 22:12:31 +0530

    Converting case into its inverse

---
+++ case_convertion.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+print '-------------------------------------------------------Case convertion---------------------------------------------------------'
+
+given_string = str(raw_input ('\nEnter a string : \t'))
+
+print '\nThe given string is case converted as follows\n\n', given_string.swapcase()
